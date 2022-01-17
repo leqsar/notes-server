4a56108e26d58098c72c5a31ab767541761f3f4d
leqsar <arteg132579@gmail.com>
2022-01-17 21:18:22 +0300

    set cors

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 5000;
 const fs = require("fs");
 const bodyParser = require('body-parser');
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, POST, PUT');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-
-  next();
-});
+app.use(cors())
 
 const urlencodedParser = express.urlencoded({extended: false});
 app.use(bodyParser.json());
