5397a2b8263083718d47835c6959e978f6fcf244
leqsar <arteg132579@gmail.com>
2022-01-17 19:25:53 +0300

    add server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const fs = require("fs");
+const bodyParser = require('body-parser');
+
+const urlencodedParser = express.urlencoded({extended: false});
+app.use(bodyParser.json());
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.get('/express_backend', (req, res) => {
+  let fileContent = fs.readFileSync("notesData.json", "utf8");
+  res.send({data: fileContent});
+});
+
+app.post('/express_backend_2', urlencodedParser, (req, res) => {
+  if(!req.body) return response.sendStatus(400);
+  let fileContent = fs.appendFileSync("notesData.json", JSON.stringify(req.body));
+});
+
+app.post('/express_backend_delete', urlencodedParser, (req, res) => {
+  if(!req.body) return response.sendStatus(400);
+  fs.writeFile("notesData.json", JSON.stringify(req.body), function(error){
+    res.send(req.body)
+  });
+});
