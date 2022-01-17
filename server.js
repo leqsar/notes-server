57c7ea35b8c047d4f3bc5a47315e10b9d277937a
leqsar <arteg132579@gmail.com>
2022-01-17 20:54:45 +0300

    add headets

--- server.js
+++ server.js
@@ -4,15 +4,22 @@ const port = process.env.PORT || 5000;
 const fs = require("fs");
 const bodyParser = require('body-parser');
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'https://naughty-johnson-cd5eb4.netlify.app/');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+    next();
+});
+
 const urlencodedParser = express.urlencoded({extended: false});
 app.use(bodyParser.json());
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-/*app.get('/express_backend', (req, res) => {
+app.get('/express_backend', (req, res) => {
   let fileContent = fs.readFileSync("notesData.json", "utf8");
   res.send({data: fileContent});
-});*/
+});
 
 app.post('/express_backend_2', urlencodedParser, (req, res) => {
   if(!req.body) return response.sendStatus(400);
